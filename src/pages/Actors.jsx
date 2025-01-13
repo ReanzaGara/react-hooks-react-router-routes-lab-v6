70e3367ed690fbbe980f32c1eaea89ddbaa230af
+ <+>
2025-01-13 12:12:19 -0600

    complete

--- src/pages/Actors.jsx
+++ src/pages/Actors.jsx
@@ -1,13 +1,32 @@
+import { getActiveElementOrBody } from "@testing-library/user-event/dist/cjs/utils/index.js";
 import { useEffect, useState } from "react";
 
 function Actors() {
+  const [actors, setActors] = useState([])
+
+  useEffect(() => {
+    fetch('http://localhost:4000/actors')
+    .then(response => response.json())
+    .then(data => setActors(data))
+    .catch(error => console.error(error))
+
+    const actor = actors.map(actor => {
+      return (
+        <article key={actor.id}>
+          <h2>{actor.name}</h2>
+          <ul>{actor.movies.map(movie => <li key={movie}>{movie}</li>)}</ul>
+        </article>
+      )
+    })
+  })
   return (
     <>
       <header>
-        {/* What component should go here? */}
+        <NavBar />
       </header>
       <main>
-        {/* Actor info here! */}
+        <h1>Actors Page</h1>
+        {actor}
       </main>
     </>
   );
--- src/pages/Directors.jsx
+++ src/pages/Directors.jsx
@@ -1,13 +1,31 @@
 import { useEffect, useState } from "react";
 
 function Directors() {
+  const [directors, setDirectors] = useState([])
+
+  useEffect(() => {
+    fetch("http://localhost:4000/directors")
+    .then(response => response.json())
+    .then(data => setDirectors(data))
+    .catch(error => console.error(error))
+  }, [])
+
+  const director = directors.map(director => {
+    return (
+      <article key={director.id}>
+        <h2>{director.name}</h2>
+        <ul>{director.movies.map(movie => <li key={movie}>{movie}</li>)}</ul>
+      </article>
+    )
+  })
   return (
     <>
       <header>
-        {/* What component should go here? */}
+        <NavBar />
       </header>
       <main>
-        {/* Director info here! */}
+        <h1>Directors Page</h1>
+        {director}
       </main>
     </>
   );
